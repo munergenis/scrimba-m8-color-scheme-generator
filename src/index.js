d5936931315e0fb13aa96caeef3f19e68943ff63
munergenis <154993411+munergenis@users.noreply.github.com>
2024-04-05 19:11:45 +0200

    index.js refactoring

--- src/index.js
+++ src/index.js
@@ -3,27 +3,28 @@ const inputModeEl = document.querySelector("#input-mode");
 const colorDisplayElArray = document.querySelectorAll(".color-display");
 const colorValElArray = document.querySelectorAll(".color-val");
 
+// Event listeners
 document.addEventListener("click", (e) => {
   if (e.target.id === "get-btn") {
-    const selectedColor = getHexColorNumber();
-    const selectedMode = inputModeEl.value;
-    fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}`)
-      .then(resp => resp.json())
-      .then(data => {
-        for (const color in data.colors) {
-          colorDisplayElArray[color].style.backgroundColor = data.colors[color].hex.value;
-          colorValElArray[color].textContent = data.colors[color].hex.value;
-        }
-      });
+    handleGetColorsClick();
   } else if (e.target.dataset.color) {
-    const selectedColor = e.target.dataset.color;
-    const selectedColorEl = document.querySelector(`#copied-${selectedColor}`);
-    selectedColorEl.classList.remove("copied-modal-hidden");
-    setTimeout(() => {
-      selectedColorEl.classList.add("copied-modal-hidden");
-    }, 1500);
+    copyColorToClipboard(e.target.dataset.color);
   }
 });
+
+// Functions
+function handleGetColorsClick() {
+  const selectedColor = getHexColorNumber();
+  const selectedMode = inputModeEl.value;
+  fetch(`https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}`)
+    .then(resp => resp.json())
+    .then(data => {
+      for (const color in data.colors) {
+        colorDisplayElArray[color].style.backgroundColor = data.colors[color].hex.value;
+        colorValElArray[color].textContent = data.colors[color].hex.value;
+      }
+    });
+}
 
 function getHexColorNumber() {
   let hexValue = inputColorEl.value;
@@ -31,4 +32,17 @@ function getHexColorNumber() {
   hexValue.shift();
   hexValue = hexValue.join("");
   return hexValue;
+}
+
+function copyColorToClipboard(color) {
+  // handle copy to clipboard
+  showModalInColor(color);
+}
+
+function showModalInColor(color) {
+  const selectedColorEl = document.querySelector(`#copied-${color}`);
+  selectedColorEl.classList.remove("copied-modal-hidden");
+  setTimeout(() => {
+    selectedColorEl.classList.add("copied-modal-hidden");
+  }, 1500);
 }
